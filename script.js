3fa8f6226af0e99a9bd410733dddfc5517dcdcae
Sujithab06 <164987345+Sujithab06@users.noreply.github.com>
2024-03-27 17:19:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let displayValue = '';
+
+function appendToDisplay(value) {
+  displayValue += value;
+  updateDisplay();
+}
+
+function clearDisplay() {
+  displayValue = '';
+  updateDisplay();
+}
+
+function updateDisplay() {
+  document.querySelector('.display').value = displayValue;
+}
+
+function calculate() {
+  try {
+    displayValue = eval(displayValue);
+    updateDisplay();
+  } catch (error) {
+    displayValue = 'Error';
+    updateDisplay();
+  }
+}
